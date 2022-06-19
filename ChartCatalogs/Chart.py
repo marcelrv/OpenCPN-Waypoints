999513134234b46922681f77e58e6bd7c3066f36
Marcel Verpaalen <marcel@verpaalen.com>
2022-06-19 15:54:18 +0200

    add chartCatalog for easy loading

---
+++ ChartCatalogs/Chart.py
@@ -0,0 +1,64 @@
+"""Chart
+Part of the ChartCatalogs project
+Copyright (c) 2015 Pavel Kalian
+Licensed under GPLv2 or, at yoir will later version
+"""
+
+from xml.etree.ElementTree import Element, SubElement, tostring
+
+class Chart:
+    def __init__(self):
+        self.number = 0
+        self.title = ''
+        self.url = ''
+        self.ntm_edition_last_correction = None
+        self.vertical_datum = None
+        self.horizontal_datum = None
+        self.chart_format = None
+        self.zipfile_ts = None
+        self.zipfile_size = 0
+        self.target_filename = None
+
+    def is_valid(self):
+        return self.number != 0 and self.title != '' and self.url != '' and self.zipfile_ts != None
+
+    def append_xml_element(self, parent, chart_name_include_number = False):
+        e = SubElement(parent, 'chart')
+        child = SubElement(e, 'number')
+        child.text = self.number
+        child = SubElement(e, 'title')
+        if chart_name_include_number:
+            child.text = self.title + " (" + self.number + ")"
+        else:
+            child.text = self.title
+        if self.vertical_datum != None:
+            child = SubElement(e, 'vertical_datum')
+            child.text = self.vertical_datum
+        if self.horizontal_datum != None:
+            child = SubElement(e, 'horizontal_datum')
+            child.text = self.horizontal_datum
+        if self.chart_format != None:
+            child = SubElement(e, 'format')
+            child.text = self.chart_format
+        child = SubElement(e, 'zipfile_location')
+        child.text = self.url
+        child = SubElement(e, 'zipfile_datetime')
+        child.text = self.zipfile_ts.strftime('%Y%m%d_%H%M%S')
+        child = SubElement(e, 'zipfile_datetime_iso8601')
+        child.text = self.zipfile_ts.strftime('%Y-%m-%dT%H:%M:%SZ')
+        if self.zipfile_size > 0:
+            child = SubElement(e, 'zipfile_size')
+            child.text = self.zipfile_size
+        if self.ntm_edition_last_correction != None:
+            child = SubElement(e, 'ntm_edition_last_correction')
+            child.text = self.ntm_edition_last_correction
+        if self.target_filename != None:
+            child = SubElement(e, 'target_filename')
+            child.text = self.target_filename
+
+    def print_info(self):
+        print (self.number)
+        print (self.title)
+        print (self.url)
+        print (self.ntm_edition_last_correction)
+
---
+++ ChartCatalogs/RncChartCatalog.py
@@ -0,0 +1,58 @@
+"""ChartCatalog
+Part of the ChartCatalogs project
+Copyright (c) 2015 Pavel Kalian
+Licensed under GPLv2 or, at yoir will later version
+"""
+
+
+from xml.etree.ElementTree import Element, SubElement, tostring
+from datetime import datetime
+
+
+class RncChartCatalog:
+    def __init__(self):
+        self.charts = []
+        self.title = ''
+        self.created_ts = datetime.utcnow()
+        self.valid_ts = datetime.utcnow()
+        self.ref_spec = 'Subset of NOAA Rnc Product Catalog Technical Specifications'
+        self.ref_spec_vers = '1.0'
+        self.s62AgencyCode = '0'
+        self.xml = None
+
+    def xml_add_header(self):
+        header = SubElement(self.xml, 'Header')
+        t = SubElement(header, 'title')
+        t.text = self.title
+        t = SubElement(header, 'date_created')
+        t.text = self.created_ts.strftime('%Y-%m-%d')
+        t = SubElement(header, 'time_created')
+        t.text = self.created_ts.strftime('%H:%M:%S')
+        t = SubElement(header, 'date_valid')
+        t.text = self.valid_ts.strftime('%Y-%m-%d')
+        t = SubElement(header, 'time_valid')
+        t.text = self.valid_ts.strftime('%H:%M:%S')
+        t = SubElement(header, 'dt_valid')
+        t.text = self.valid_ts.strftime('%Y-%m-%dT%H:%M:%SZ')
+        t = SubElement(header, 'ref_spec')
+        t.text = self.ref_spec
+        t = SubElement(header, 'ref_spec_vers')
+        t.text = self.ref_spec_vers
+        t = SubElement(header, 's62AgencyCode')
+        t.text = self.s62AgencyCode
+
+    def xml_add_charts(self, chart_name_include_number):
+        for chart in self.charts:
+            chart.append_xml_element(self.xml, chart_name_include_number)
+
+    def get_xml(self, chart_name_include_number=False) -> str:
+        self.xml = Element('RncProductCatalogChartCatalogs')
+        self.xml_add_header()
+        self.xml_add_charts(chart_name_include_number)
+        return tostring(self.xml, encoding='unicode')
+
+    def print_xml(self, chart_name_include_number=False):
+        print(self.get_xml(chart_name_include_number))
+
+    def add_chart(self, chart):
+        self.charts.append(chart)
